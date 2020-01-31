b9cdc72bb49a26bd1617eddc2f930930fb7f1f97
lee <q2298478324@gmail.com>
2020-01-31 20:48:43 +0900

    hw1

---
+++ hw1/src/two_view_geometry.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+#include <opencv2/opencv.hpp>
+#include<opencv2/features2d/features2d.hpp>
+#include "two_view_geometry.h"
+
+namespace TwoViewGeometry {
+
+void FindFundamental(const vector<cv::Point2f>& vPts1, const vector<cv::Point2f>& vPts2, const vector<cv::DMatch> &vMatches12, vector<bool> &vbMatchesInliers,
+                    cv::Mat &F21, float &score, float sigma, int maxIterations)
+{
+   // TODO homework
+}
+
+bool ReconstructF(const vector<cv::Point2f>& vPts1, const vector<cv::Point2f>& vPts2, const vector<cv::DMatch> &vMatches12, vector<bool> &vbMatchesInliers,
+                  cv::Mat &F21, cv::Mat &K, cv::Mat &R21, cv::Mat &t21, vector<cv::Point3f> &vP3D, vector<bool> &vbTriangulated, float minParallax, int minTriangulated, float sigma)
+{
+   // TODO homework
+    return false;
+}
+
+}
